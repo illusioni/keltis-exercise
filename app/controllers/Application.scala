8f6ec0e1cce6a90c81a2f6c77e0edb9bf17af396
illusioni <klaus@illusioni.de>
2012-08-21 22:43:53 +0200

    added a test Action, a route, and a link on top of the index page

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,11 +2,19 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import play.api.libs.iteratee.Enumerator
 
 object Application extends Controller {
   
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
+  }
+  
+  def test = Action {
+  	SimpleResult(
+  		header = ResponseHeader(200, Map(CONTENT_TYPE -> "text/plain")),
+  		body = Enumerator("This is a test!")
+  	)
   }
   
 }
